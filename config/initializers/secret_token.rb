9cd5c8e030d3ec1765327ef1d45b8fc7f113e87c
Hyunwoo Cho <hc27oclock@gmail.com>
2011-06-14 15:51:03 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ontol::Application.config.secret_token = 'c002416eab540ee6bbdc918147ade54e33ea2fcd8143165274bc455165e481152314ebd42e44ec782fb1cff74d343467f400d8c4819d0148a4560c48f6c7e38a'
